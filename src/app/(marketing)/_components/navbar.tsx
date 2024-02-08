fe016655edb8a1d860f56fb853e2c9341252cd48
breanna-rayburn <breanna.rayburn00@gmail.com>
2024-02-07 20:41:02 -0800

    fix: navbar buttons now space between on displays smaller than md

--- src/app/(marketing)/_components/navbar.tsx
+++ src/app/(marketing)/_components/navbar.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link'
 
 export const Navbar = () => {
   return (
-    <nav className="sticky flex h-14 w-full">
+    <nav className="sticky flex h-14 w-full border-b px-4 shadow-sm">
       <header className="mx-auto flex w-full items-center justify-between md:max-w-screen-md">
         <div className="hidden md:flex">
           <Link href="/">
             <h2>Plannrly</h2>
           </Link>
         </div>
-        <div className="flex space-x-4 md:block md:w-auto">
+        <div className="flex w-full items-center justify-between space-x-4 md:block md:w-auto">
           <Button variant="outline" asChild>
             <Link href="/sign-in">Login</Link>
           </Button>
